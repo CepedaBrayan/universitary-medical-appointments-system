0a9663a32cac0b0455055da1032a8cdbc39f030f
Brayan Rivera Cepeda <riveracepedabrayan@gmail.com>
2022-07-14 19:51:25 -0500

    patch student endpoint

--- src/students/dto/update-student.dto.ts
+++ src/students/dto/update-student.dto.ts
@@ -1,4 +1,13 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
+import { IntersectionType, OmitType } from '@nestjs/swagger';
 import { CreateStudentDto } from './create-student.dto';
 
-export class UpdateStudentDto extends PartialType(CreateStudentDto) {}
+export class AdditionalAuthToken {
+  @ApiProperty({ type: String, description: 'nickname' })
+  auth_token: string;
+}
+
+export class UpdateStudentDto extends IntersectionType(
+  CreateStudentDto,
+  AdditionalAuthToken,
+) {}
